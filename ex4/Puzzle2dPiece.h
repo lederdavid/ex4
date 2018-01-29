eb3a11d233d4595cd47eebf607ce37861b89c16d
lederdavid <lederkremer@gmail.com>
2018-01-29 21:27:42 +0200

    changed design made PuzzlePieceBase class

--- ex4/Puzzle2dPiece.h
+++ ex4/Puzzle2dPiece.h
@@ -3,21 +3,25 @@
 #define PUZZLE2_DPIECE_H_INCLUDED   
 #include <iostream>
 #include <cassert>
+#include "PuzzlePieceBase.h"
 using namespace std;
 
 template <int T>
-class Puzzle2dPiece
+class Puzzle2dPiece : public PuzzlePieceBase
 {
 public:
 	Puzzle2dPiece(int a, int b, int c, int d);
-	int _edges[4]{};
-	void to_string();
-	int _t;
+	//int _edges[4]{};
+	vector<int> get_edges() override;
+	void to_string() override;
+private:
+	PuzzlePieceBase* base{};
+	vector<int> _edges;
 };
 
 
 template <int T>
-Puzzle2dPiece<T>::Puzzle2dPiece(const int a, const int b, const int c, const int d) :_t(T)
+Puzzle2dPiece<T>::Puzzle2dPiece(const int a, const int b, const int c, const int d) : PuzzlePieceBase(T)
 {
 	assert(-1 * T <= a && a <= T);
 	assert(-1 * T <= b && b <= T);
@@ -27,6 +31,13 @@ Puzzle2dPiece<T>::Puzzle2dPiece(const int a, const int b, const int c, const int
 	_edges[1] = b;
 	_edges[2] = c;
 	_edges[3] = d;
+}
+
+
+template <int T>
+vector<int> Puzzle2dPiece<T>::get_edges()
+{
+	return _edges;
 }
 
 template <int T>
