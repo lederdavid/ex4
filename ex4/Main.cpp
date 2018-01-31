cebde3e1cde0c673b093994d3e1ff6405c8294ac
lederdavid <lederkremer@gmail.com>
2018-01-31 12:57:56 +0200

    bah

--- ex4/Main.cpp
+++ ex4/Main.cpp
@@ -5,7 +5,7 @@
 int main() {
 	auto joker = std::numeric_limits<int>::min();
 	list<Puzzle3dPiece<1>> pieces = { { 0, 1, 1, 1, -1, -1 },{ 0, -1, 1, 1, 1, 1 } };
-	auto& groups = groupPuzzlePieces(pieces.begin(), pieces.end());
+	auto groups = groupPuzzlePieces(pieces.begin(), pieces.end());
 	auto some_pieces = groups.get({ 0, joker, 1, 1, joker, joker });
 	for (auto piece_ptr : some_pieces) {
 		std::cout << *piece_ptr << std::endl; // will print two pieces!
